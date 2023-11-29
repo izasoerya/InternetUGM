4a797af487725bd673f483b023a21542a7743b38
izasoerya <ihza.surya@gmail.com>
2023-11-29 11:53:31 +0700

    refactor

--- internet.cpp
+++ internet.cpp
@@ -12,16 +12,19 @@ int InternetUGM::begin(WiFiClientSecure &client, HTTPClient &http){
     WiFi.mode(WIFI_STA); 
     WiFi.begin(ssid);
     
-    while (WiFi.status() != WL_CONNECTED);
-    Serial.println(WiFi.localIP());
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    uint8_t lenUsername = username.length();
+    uint8_t lenPassword = password.length();
 
-    char jsonString[255];
+    char jsonString[72 + lenUsername + lenPassword];
     snprintf(jsonString, sizeof(jsonString),
              "{\"username\":\"%s\",\"password\":\"%s\",\"_eventId\":\"submit\",\"submit\":\"login\"}",
              username, password);
 
     client.setInsecure();
-
     http.begin(client, url);
     int httpResponseCode = http.POST(jsonString);
     http.end();
